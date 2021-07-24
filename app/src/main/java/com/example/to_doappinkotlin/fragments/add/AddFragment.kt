a96a9b277aeccfbda7eae950e5f15fe094832104
Jacob Reola <jacobreola@gmail.com>
2021-07-24 13:37:29 -0500

    fixed add xml and gradle

--- app/src/main/java/com/example/to_doappinkotlin/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/to_doappinkotlin/fragments/add/AddFragment.kt
@@ -14,7 +14,7 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
 }
