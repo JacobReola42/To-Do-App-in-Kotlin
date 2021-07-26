8e160f6e1095c4cef390a5bce2b49f3bb0b1af7f
Jacob Reola <jacobreola@gmail.com>
2021-07-26 15:16:17 -0500

    SharedViewModel.kt for manual database check

---
+++ app/src/main/java/com/example/to_doappinkotlin/fragments/SharedViewModel.kt
@@ -0,0 +1,68 @@
+package com.example.to_doappinkotlin.fragments
+
+import android.app.Application
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.to_doappinkotlin.R
+import com.example.to_doappinkotlin.data.models.Priority
+import com.example.to_doappinkotlin.data.models.ToDoData
+
+/* originally for AddFragment.k */
+
+class SharedViewModel(application: Application): AndroidViewModel(application) {
+
+    /** ============================= List Fragment ============================= */
+
+    val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    fun checkIfDatabaseEmpty(toDoData: List<ToDoData>){
+        emptyDatabase.value = toDoData.isEmpty()
+    }
+
+    /** ============================= Add/Update Fragment ============================= */
+
+    val listener: AdapterView.OnItemSelectedListener = object :
+        AdapterView.OnItemSelectedListener{
+        override fun onNothingSelected(p0: AdapterView<*>?) {}
+        override fun onItemSelected(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ) {
+            when(position){
+                0 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.red)) }
+                1 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.yellow)) }
+                2 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.green)) }
+            }
+        }
+    }
+
+    /* Needed to manually check data on SQLite database. for DeviceFile explorer to
+       data > data > com.example.to_doappinkotlin > databases > *. Open and check with
+       SQLite and go to browse data > to do table when using todo_database file.
+       For reading data. NEED to use SQLite Browser.
+     */
+    fun verifyDataFromUser(title: String, description: String): Boolean {
+        return !(title.isEmpty() || description.isEmpty())
+    }
+
+    /* Needed to manually check data on SQLite database. for DeviceFile explorer to
+       data > data > com.example.to_doappinkotlin > databases > *. Open and check with
+       SQLite and go to browse data > to do table when using todo_database file.
+       For reading data. NEED to use SQLite Browser.
+     */
+    fun parsePriority(priority: String): Priority {
+        return when(priority){
+            "High Priority" -> { Priority.HIGH }
+            "Medium Priority" -> { Priority.MEDIUM }
+            "Low Priority" -> { Priority.LOW }
+            else -> Priority.LOW
+        }
+    }
+
+}
